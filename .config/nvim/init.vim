51e6d9ff3907f3d1d2bdcf34399b61390aaa9edf
Luke Bocker <lukeom6@web.de>
2022-06-02 11:32:56 +0200

    dracula alcritty and nvim plugins

---
+++ .config/nvim/init.vim
@@ -0,0 +1,7 @@
+call plug#begin()
+Plug 'preservim/nerdtree'
+Plug 'vim-syntastic/syntastic'
+Plug 'ycm-core/YouCompleteMe'
+Plug 'myusuf3/numbers.vim'
+Plug 'dracula/vim', { 'as': 'dracula' }
+call plug#end()
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
-sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
+sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev neovim 
 sudo apt-get install ninja-build
 
 cd /usr/share/fonts/
@@ -37,12 +37,14 @@ mkdir -p ~/.config/compton
 mkdir -p ~/.config/rofi
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/picom
+mkdir -p ~/.config/nvim
 cp .config/i3/config ~/.config/i3/config
 cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 cp .config/compton/compton.conf ~/.config/compton/compton.conf
 cp .config/rofi/config ~/.config/rofi/config
 cp .config/picom/picom.conf ~./config/picom/picom.conf
+cp .config/nvim/init.vim ~/.config/nvim/init.vim
 cp .fehbg ~/.fehbg
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
